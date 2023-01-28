a3c586c52248922a01d0247170a493590c828d40
Philipuuu <topgun920415@gmail.com>
2023-01-28 14:32:30 +0900

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from scraper import Scraper
-
-app = FastAPI()
-scraper = Scraper()
-
-@app.get("/{word}")
-async def search_google(word):
-    return scraper.google_search(word)
